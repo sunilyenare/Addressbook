7a297242841237738e7fa642b12af09578222f78
sunilsyenare <syenare97@gmail.com>
2019-09-25 23:55:36 +0530

    [sunil] Add. create book implementation

---
+++ src/main/java/com/addressbook/test/AddressBook.java
@@ -0,0 +1,15 @@
+package com.addressbook.test;
+
+public class AddressBook {
+
+    private String bookName;
+
+    public AddressBook(String bookName) {
+        this.bookName=bookName;
+    }
+
+
+    public boolean create() {
+        return true;
+    }
+}
