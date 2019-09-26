f9c61b822a4dd9e413dfd649b1ee8dd6a39952c1
sunilsyenare <syenare97@gmail.com>
2019-09-26 15:41:21 +0530

    Refactor. Remove unnecessary interface Address. Change home to address

--- src/main/java/com/addressbook/address/Address.java
+++ src/main/java/com/addressbook/address/Address.java
@@ -3,6 +3,20 @@ package com.addressbook.address;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public interface Address {
-    String address();
+public class Address {
+    private String landmark;
+    private String city;
+    private String state;
+    private int zip;
+
+    public Address(String landmark, String city, String state, int zip) {
+        this.landmark = landmark;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public String address() {
+        return landmark + city + state + zip;
+    }
 }
--- src/main/java/com/addressbook/book/Main.java
+++ src/main/java/com/addressbook/book/Main.java
@@ -2,14 +2,13 @@ package com.addressbook.book;
 
 import com.addressbook.Person;
 import com.addressbook.address.Address;
-import com.addressbook.address.Home;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
 
 import java.io.IOException;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class Main {
     public static void main(String args[]) throws IOException {
-        Address home = new Home("234,HSTR ", "Mysoree", "Karnataka", 560076);
+        Address home = new Address("234,HSTR ", "Mysoree", "Karnataka", 560076);
         Person person = new Person("Sunil Yenare", "Yenare", "0987654321", home);
         AddressBook addressBook = new Add("Bangalore", person);
         addressBook.execute();
